3e5756ddc263a745d0855091a5b97094b57c3de7
nohandev <nohanpablo759@gmail.com>
2025-11-16 20:38:21 -0300

    feat: start page

---
+++ src/components/UI/Button.tsx
@@ -0,0 +1,22 @@
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  icon?: React.ReactNode
+  children: React.ReactNode
+  className?: string
+}
+
+const Button:React.FC<ButtonProps> = ({
+  children, 
+  icon, 
+  className='',
+  ...props}) => {
+  return (
+   <button 
+   className={`py-4  px-2 text-lg cursor-pointer flex items-center justify-center gap-2 ${className}`}
+   {...props}>
+    {icon && <span>{icon}</span>}
+    {children}
+   </button>
+  )
+}
+
+export default Button
