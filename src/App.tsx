614b98749ce36c8bfa31d81aa47ec7a049749df9
Arlesson-Sales <urameshy11@gmail.com>
2025-11-16 14:58:46 -0300

    first project commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { type JSX } from "react";
+
+export default function App(): JSX.Element
+{
+    return (
+        <div>
+            <h1>knowledge Quiz Challenge</h1>
+        </div>
+    );
+}
